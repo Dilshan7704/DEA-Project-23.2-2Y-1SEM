0876e4680f57300661363ce0369422fd5ac8c29f
Dilshan7704 <dilshannethmin77@gmail.com>
2025-05-17 20:52:11 +0530

    Fixed dao

--- DEA Group Project/src/java/dao/ProjectDAO.java
+++ DEA Group Project/src/java/dao/ProjectDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -38,5 +39,31 @@ public class ProjectDAO {
         }
         
         return projectList;
+    }
+    public List<Project> getProjectsByClientId(int clientId) {
+        List<Project> projects = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM project WHERE client_id = ?";
+            Connection conn = DB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, clientId);
+            
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Project project = new Project();
+                project.setProject_id(rs.getInt("project_id"));
+                project.setProject_name(rs.getString("project_name"));
+                project.setProject_description(rs.getString("project_description"));
+                project.setProject_startdate(rs.getDate("project_startdate").toLocalDate());
+                project.setProject_enddate(rs.getDate("project_enddate").toLocalDate());
+                project.setStatus(rs.getString("status"));
+                project.setProject_budget(rs.getFloat("project_budget"));
+                project.setClient_id(rs.getInt("client_id"));
+                projects.add(project);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return projects;
     }
 }
