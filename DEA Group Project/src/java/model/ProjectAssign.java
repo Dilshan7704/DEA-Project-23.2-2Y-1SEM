1b56624143ab273e05ecabfbdcdd9f79444dc91f
Gihan <gihanmadusanka875@gmail.com>
2025-05-17 21:18:08 +0530

    Create ProjectAssign model

---
+++ DEA Group Project/src/java/model/ProjectAssign.java
@@ -0,0 +1,54 @@
+
+package model;
+
+/**
+ *
+ * @author gihan
+ */
+public class ProjectAssign {
+
+    /**
+     * @return the assign_id
+     */
+    public int getAssign_id() {
+        return assign_id;
+    }
+
+    /**
+     * @param assign_id the assign_id to set
+     */
+    public void setAssign_id(int assign_id) {
+        this.assign_id = assign_id;
+    }
+
+    /**
+     * @return the project_id
+     */
+    public int getProject_id() {
+        return project_id;
+    }
+
+    /**
+     * @param project_id the project_id to set
+     */
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+
+    /**
+     * @return the employee_id
+     */
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    /**
+     * @param employee_id the employee_id to set
+     */
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+       private int assign_id;
+    private int project_id;
+    private int employee_id;
+}
