a34c71639a110b6fb87dc05eee134808e6124ac3
Dilshan7704 <dilshannethmin77@gmail.com>
2025-05-17 16:56:14 +0530

    AdminDAO.java file  added

---
+++ DEA Group Project/src/java/dao/AdminDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import com.nsbm.model.Admin;
+import com.nsbm.util.DB;
+import java.sql.*;
+
+public class AdminDAO {
+    public Admin validateAdmin(String email, String password) {
+        Admin admin = null;
+        try {
+            String sql = "SELECT * FROM admin WHERE admin_email = ? AND admin_password = ?";
+            Connection conn = DB.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                admin = new Admin();
+                admin.setAdminId(rs.getInt("admin_id"));
+                admin.setEmail(rs.getString("admin_email"));
+                admin.setPassword(rs.getString("admin_password"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return admin;
+    }
+}
