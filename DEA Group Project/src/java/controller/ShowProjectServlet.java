7c0f2fc5603f1f78b02737a8fa1bf81f132b9109
Gihan <gihanmadusanka875@gmail.com>
2025-05-17 19:33:37 +0530

    Create ShowProjectServlet.java

---
+++ DEA Group Project/src/java/controller/ShowProjectServlet.java
@@ -0,0 +1,29 @@
+
+package controller;
+
+import com.project.dao.AddProjectDAO;
+import com.project.model.Project;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/projectList")
+public class ShowProjectServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        AddProjectDAO projectDAO = new AddProjectDAO();
+        List<Project> projectList = projectDAO.ProjectList();
+
+        request.setAttribute("projectList", projectList); // pass to JSP
+        request.getRequestDispatcher("projectList.jsp").forward(request, response);
+    }
+}
+
